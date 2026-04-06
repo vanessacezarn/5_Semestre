d9fc483870e686caafc7d38ed0cd4e9a8f89e8be
Vanessa Cezar <vanessacezar3449@gmail.com>
2026-04-06 10:38:44 -0300

    Add files via upload

--- Projeto de Software/crud/src/main/java/com/example/crud/controller/ProdutoController.java
+++ Projeto de Software/crud/src/main/java/com/example/crud/controller/ProdutoController.java
@@ -2,22 +2,23 @@ package com.example.crud.controller;
 
 import com.example.crud.model.Produto;
 import com.example.crud.repository.ProdutoRepository;
+import com.example.crud.service.ProdutoService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/produto")
 public class ProdutoController {
     private final ProdutoRepository produtoRepository;
+    private final ProdutoService produtoService;
 
-    public ProdutoController(ProdutoRepository produtoRepository) {
+    public ProdutoController(ProdutoRepository produtoRepository, ProdutoService produtoService) {
         this.produtoRepository = produtoRepository;
+        this.produtoService = produtoService;
     }
 
     @GetMapping("/formulario")
@@ -28,7 +29,7 @@ public class ProdutoController {
 
     @PostMapping("/salvar")
     public String salvarProduto(@ModelAttribute Produto produto) {
-        produtoRepository.save(produto);
+        produtoService.salvar(produto);
         return "redirect:/produto/listar";
     }
 
@@ -38,5 +39,20 @@ public class ProdutoController {
         model.addAttribute("produtos", produtos);
         return "lista";
     }
+
+    @GetMapping("/deletar/{id}")
+    public String deletarProduto(@PathVariable Integer id) {
+        /*PathVariable = variavel de caminho, ela esta vindo junto com url*/
+        produtoRepository.deleteById(id);
+        return "redirect:/produto/listar";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String editarProduto(@PathVariable Integer id, Model model) {
+        Optional<Produto> produto = produtoRepository.findById(id);
+        model.addAttribute("produto", produto);
+        return "formulario";
+    }
+
 
 }
---
+++ Projeto de Software/crud/src/main/java/com/example/crud/service/ProdutoService.java
@@ -0,0 +1,24 @@
+package com.example.crud.service;
+
+import com.example.crud.model.Produto;
+import com.example.crud.repository.ProdutoRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProdutoService {
+     private final ProdutoRepository produtoRepository;
+
+     public ProdutoService(ProdutoRepository produtoRepository) {
+          this.produtoRepository = produtoRepository;
+     }
+     public void salvar(Produto produto) {
+          if(produto.getNome() == null || produto.getNome().isBlank()) {
+               throw new RuntimeException("O nome do produto é obrigatório");
+          }
+          if(produto.getValor() <= 0){
+               throw new RuntimeException("o valor deve ser maior que 0");
+          }
+          produtoRepository.save(produto);
+     }
+
+}
