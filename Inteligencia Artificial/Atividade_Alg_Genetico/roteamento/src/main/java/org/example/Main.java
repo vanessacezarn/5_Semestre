f026246829bad5d4879c435c1b639f0fe55ab838
Vanessa Cezar <vanessacezar3449@gmail.com>
2026-05-07 10:07:51 -0300

    Update Main.java

--- Inteligencia Artificial/Atividade_Alg_Genetico/roteamento/src/main/java/org/example/Main.java
+++ Inteligencia Artificial/Atividade_Alg_Genetico/roteamento/src/main/java/org/example/Main.java
@@ -5,10 +5,8 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
+    public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Tamanho da população: ");
